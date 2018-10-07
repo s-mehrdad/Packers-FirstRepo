daf6c92f939a445629a921db2533e24ce13f1ca0
Mehrdad Solimanimajd <s.mehrdad.47@gmail.com>
2018-10-07 13:56:52 +0330

    some fundamental change

---
+++ ConsoleAdjustments.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void ConsoleFont_SizeColour (COORD, LPCWSTR, WORD);
+void ConsoleScreen_SizeColourPosition (COORD, COORD, COLORREF);
+void ConsoleCursor_State (bool);
+void ColourCout (std::string, WORD);
